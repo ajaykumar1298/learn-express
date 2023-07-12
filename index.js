14f7039a9ea2631e133eefe144a55dcf2766c7f9
Ajay Kumar <ajaykumar@spicegems.com>
2023-07-12 19:16:31 +0530

    use routes

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path=require("path")
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use("/",require(path.join(__dirname,"routes/blog.js")))
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
