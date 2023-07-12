fe40ada625340983673ceb7d08ce3ab9ad916bc5
ajaykumar1298 <ajaykumarak1209@gmail.com>
2023-07-12 23:10:08 +0530

    data/blogs add structure

---
+++ data/blogs.js
@@ -0,0 +1,19 @@
+blogs = [
+  {
+    title: "this is the blog of js",
+    desc: "this is desc part of js",
+  },
+  {
+    title: "this is the blog of css",
+    desc: "this is desc part of css",
+  },
+  {
+    title: "this is the blog of php",
+    desc: "this is desc part of php",
+  },
+  {
+    title: "this is the blog of react",
+    desc: "this is desc part of react",
+  },
+];
+module.exports = blogs;
--- routes/blog.js
+++ routes/blog.js
@@ -1,10 +1,18 @@
-const express =require("express")
-const route=express.Router()
-const path=require("path")
+const express = require("express");
+const route = express.Router();
+const path = require("path");
+const blogs = require("../data/blogs");
 
-route.get("/",(req,res)=>{
+route.get("/", (req, res) => {
   // res.send("hey")
-  res.sendFile(path.join(__dirname,"../templates/index.html"))
-})
+  res.sendFile(path.join(__dirname, "../templates/index.html"));
+});
+route.get("/blogs", (req, res) => {
+  // res.send("hey")
+  // res.sendFile(path.join(__dirname, "../templates/index.html"));
+  blogs.forEach((elem) => {
+    console.log(elem.title);
+  });
+});
 
-module.exports=route
+module.exports = route;
